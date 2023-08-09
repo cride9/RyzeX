599284ff053c5da4bc53ed11b503e32461840007
Michtar <59453471+Michtarr@users.noreply.github.com>
2023-08-09 12:27:41 +0200

    Fix skinchanger WIP

--- RyzeXTR/Core/Interface/Interfaces/IItemSystem.h
+++ RyzeXTR/Core/Interface/Interfaces/IItemSystem.h
@@ -44,6 +44,27 @@ struct Node {
 template <typename Key, typename Value>
 struct UtlMap
 {
+	auto begin() const noexcept { return memory.memory; }
+	auto end() const noexcept { return memory.memory + numElements; }
+
+	int find(Key key) const noexcept
+	{
+		auto curr = root;
+
+		while (curr != -1)
+		{
+			const auto el = memory[curr];
+
+			if (el.key < key)
+				curr = el.right;
+			else if (el.key > key)
+				curr = el.left;
+			else
+				break;
+		}
+		return curr;
+	}
+
 	void* lessFunc;
 	UtlMemory<Node<Key, Value>> memory;
 	int root;
